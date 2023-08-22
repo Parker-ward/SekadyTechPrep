f32217f8f7f263241d7578bd87a697c27b8f3cd6
Parker <ward.parker26@gmail.com>
2023-08-22 14:12:22 -0600

    enum done

--- SekadyTechPrep/server/models/itineraryItems.js
+++ SekadyTechPrep/server/models/itineraryItems.js
@@ -6,6 +6,7 @@ export const itineraryItemsSchema = new Schema({
   confirmationNumber: { type: String, required: true, minLength: 10, maxLength: 10 },
   address: { type: String, required: true, },
   date: { type: String, required: true, },
-  cost: { type: Number, required: true, },
-
+  price: { type: Number, required: true, },
+  creatorId: { type: String, required: true },
+  tripType: { type: String, required: true, minLength: 3, maxLength: 500, enum: ['plane', 'car', 'beach', 'themepark', 'hotel', 'condo', 'house', 'bus', 'car'] }
 })
