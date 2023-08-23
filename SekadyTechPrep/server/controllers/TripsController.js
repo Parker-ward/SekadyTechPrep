20e7da8de7f006bee951c1ff642c6e49cfa68374
Parker <ward.parker26@gmail.com>
2023-08-23 12:52:59 -0600

    createTrip (backend)

---
+++ SekadyTechPrep/server/controllers/TripsController.js
@@ -0,0 +1,30 @@
+import { tripsService } from "../services/TripsService.js";
+import BaseController from "../utils/BaseController.js";
+
+export class TripsController extends BaseController {
+  constructor() {
+    super('/api/trips')
+    this.router
+      .post('', this.createTrip)
+      .get('', this.getAllTrips)
+  }
+  async getAllTrips(req, res, next) {
+    try {
+      const tripId = req.params.tripId
+      const trip = await tripsService.getAllTrips()
+      res.send(trip)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async createTrip(req, res, next) {
+    try {
+      const tripData = req.body
+      tripData.creatorId = req.userInfo._id
+      const trip = await tripsService.createTrip(tripData)
+      return res.send(trip)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- SekadyTechPrep/server/services/TripsService.js
+++ SekadyTechPrep/server/services/TripsService.js
@@ -1,10 +1,12 @@
 import { dbContext } from "../db/DbContext.js"
 
 class TripsService {
+  getAllTrips() {
+    throw new Error("Method not implemented.")
+  }
   async createTrip(tripData) {
     const trip = await dbContext.Trips.create(tripData)
     await trip.populate('creator', 'name picture')
-
     return (trip)
   }
 }
