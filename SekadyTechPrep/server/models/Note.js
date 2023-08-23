24ca3627e5ed07d94b00eb47bbeb5c17eb9a231a
Parker <ward.parker26@gmail.com>
2023-08-23 11:44:06 -0600

    note model on itinerary (backend)

--- SekadyTechPrep/server/models/Note.js
+++ SekadyTechPrep/server/models/Note.js
@@ -2,5 +2,14 @@ import { Schema } from "mongoose";
 
 export const NoteSchema = new Schema({
   body: { type: String, required: true, minLength: 3, maxLength: 200 },
-  tripId: { type: String, required: true }
+  tripId: { type: String, required: true },
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true, default: true },
+  isAttending: { type: Boolean, required: false }
+})
+
+NoteSchema.virtual('creator', {
+  ref: 'Account',
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true
 })
