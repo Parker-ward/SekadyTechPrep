f0d67fde6837044f5da07e96f0cd19b7add4c4ca
Parker <ward.parker26@gmail.com>
2023-08-25 14:38:06 -0600

    frontend up, button to create in, need modal

--- SekadyTechPrep.client/src/pages/HomePage.vue
+++ SekadyTechPrep.client/src/pages/HomePage.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="text-center">
+    <h1>Hello Home Page</h1>
+  </div>
+  <div class="d-flex justify-content-end my-3 p-2 fixed-bottom">
+    <button class="btn btn-success col-md-1" title="New trip" @click="createTrip" data-bs-toggle="modal"
+      data-bs-target="#exampleModalLable"><i class="mdi mdi-plus"></i>
+    </button>
   </div>
 </template>
 
