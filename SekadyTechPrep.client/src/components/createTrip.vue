7c59724597a6b80fb5451b06052664d2b9433ac9
Parker <ward.parker26@gmail.com>
2023-08-28 12:21:12 -0600

    editable

--- SekadyTechPrep.client/src/components/createTrip.vue
+++ SekadyTechPrep.client/src/components/createTrip.vue
@@ -1,9 +1,9 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <div class="mb-3">
-      <label for="description" class="form-label">Description</label>
-      <textarea required type="text" name="description" v-model="editable.description" class="form-control"
-        id="description" rows="3"></textarea>
+      <label for="description" class="form-label">location</label>
+      <textarea required type="text" name="location" v-model="editable.location" class="form-control" id="location"
+        rows="3"></textarea>
     </div>
     <div class="mb-3">
       <label for="name" class="form-label">Name</label>
@@ -11,19 +11,13 @@
         rows="1"></textarea>
     </div>
     <div class="mb-3">
-      <label for="coverImg" class="form-label">Cover Img</label>
-      <input required type="text" v-model="editable.coverImg" class="form-control" id="coverImg" placeholder="CoverImg..."
-        name="coverImg">
+      <label for="img" class="form-label">Image</label>
+      <input required type="text" v-model="editable.img" class="form-control" id="img" placeholder="img..." name="img">
     </div>
     <div class="mb-3">
       <label for="startDate" class="form-label">Start Date</label>
       <input required type="date" v-model="editable.startDate" class="form-control" id="startDate" placeholder=""
         name="startDate">
-    </div>
-    <div class="mb-3">
-      <label for="location" class="form-label">Location</label>
-      <input required type="text" v-model="editable.location" class="form-control" id="location" placeholder="location"
-        name="location">
     </div>
     <div class="mb-3">
       <label for="capacity" class="form-label">Capacity</label>
